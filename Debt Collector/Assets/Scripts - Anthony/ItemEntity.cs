7ff0c19b72d43b6b7f6013b62e579f4483477069
eleazar <earicojr@gmail.com>
2024-04-25 14:55:56 -0700

    Added Rotation and remove rotation upon picking up item

--- Debt Collector/Assets/Scripts - Anthony/ItemEntity.cs
+++ Debt Collector/Assets/Scripts - Anthony/ItemEntity.cs
@@ -7,22 +7,29 @@ using UnityEngine;
 public class ItemEntity : MonoBehaviour {
     public int damageMultiplier = 2;
     private int durability = 5; // will touch up more on later
+    private bool isEquipped = false;
+    private float angle = 25f;
     public GameObject pivotPoint;
     
     void Start() {
-
+        transform.rotation = Quaternion.Euler(angle, 0f, 0f);
     }
     
     void Update() {
         if (Input.GetKeyDown(KeyCode.Q)) {
             Debug.Log("Q key is pressed down");
             DropItem();
+            Unequipped();
         }
+        
+        if (!isEquipped)
+            pivotPoint.transform.rotation *= Quaternion.AngleAxis(45f * Time.deltaTime, Vector3.up);
     }
 
     void DropItem() {
         PlayerManager.itemHolder.transform.DetachChildren();
         PlayerManager.currentItem = null;
+        isEquipped = false;
         GetComponent<Collider>().enabled = true;
         
         if (CollectionManager.instance != null)
@@ -32,13 +39,21 @@ public class ItemEntity : MonoBehaviour {
         }
     }
 
+    void Unequipped() {
+        transform.localRotation = Quaternion.Euler(angle, 0f, 0f);
+    }
+
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Player") && PlayerManager.currentItem == null) {
+            isEquipped = true;
             GameObject holder = GameObject.Find("itemHolder");
+            
+            transform.localRotation = Quaternion.identity;
+            
             pivotPoint.transform.SetParent(holder.transform);
-
             pivotPoint.transform.localPosition = Vector3.zero;
             pivotPoint.transform.localRotation = Quaternion.identity;
+            
             GetComponent<Collider>().enabled = false;
             
             CollectionManager.instance.itemUpdate(gameObject);
