4bd7ed5d6ff8ec11829d3c4986cd95b3abc1da73
Ken Hlaing <kenrick0102@gmail.com>
2024-05-05 16:03:47 -0700

    Added Anthony Pause and Game Over to integration

--- Debt Collector/Assets/Anthony/Scripts - Anthony/UIManager.cs
+++ Debt Collector/Assets/Anthony/Scripts - Anthony/UIManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class UIManager : MonoBehaviour {
 
     public GameObject HUDCanvas;
+    public GameObject healthBarCanvas;
     public GameObject pauseCanvas;
     public GameObject gameOverCanvas;
     private bool isPaused = false;
@@ -23,7 +24,7 @@ public class UIManager : MonoBehaviour {
                 Pause();
         }
         
-        if (CollectionManager.totalDebt <= 0)
+        if (CollectionManager.totalDebt <= 0 || Input.GetKeyDown(KeyCode.O))
             EndGame();
     }
 
@@ -31,6 +32,7 @@ public class UIManager : MonoBehaviour {
         isPaused = true;
         Time.timeScale = 0f;
         HUDCanvas.SetActive(false);
+        healthBarCanvas.SetActive(false);
         pauseCanvas.SetActive(true);
     }
 
@@ -38,12 +40,14 @@ public class UIManager : MonoBehaviour {
         isPaused = false;
         Time.timeScale = 1f;
         HUDCanvas.SetActive(true);
+        healthBarCanvas.SetActive(true);
         pauseCanvas.SetActive(false);
     }
 
     public void EndGame() {
         Time.timeScale = 0f;
         HUDCanvas.SetActive(false);
+        healthBarCanvas.SetActive(false);
         gameOverCanvas.SetActive(true);
     }
 
